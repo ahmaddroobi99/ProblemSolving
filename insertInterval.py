3151a4646db9bcbbc564690a15d7be1673878b52
ahmaddroobi99 <ahmad.droobi1999@gmail.com>
2021-06-27 14:06:13 +0300

    fgn

--- insertInterval.py
+++ insertInterval.py
@@ -28,3 +28,22 @@ class Solution:
             else:
                 newIntervals.append(interval)
         return newIntervals
+
+
+    +++++++++++++++++++++++++++++++++++
+
+    class Solution:
+        def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+            intervals.append(newInterval)
+            intervals.sort(key=lambda intv: intv[0])
+
+            ans = []
+            ans.append(intervals[0])
+            for i in range(1, len(intervals)):
+                curr = intervals[i]
+                last = ans[-1]
+                if curr[0] <= last[1]:
+                    last[1] = max(curr[1], last[1])
+                else:
+                    ans.append(curr)
+            return ans
