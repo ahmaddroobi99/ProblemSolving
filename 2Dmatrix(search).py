ff93934adc2bc31bd8fd7361678ad02b92ced619
ahmaddroobi99 <ahmad.droobi1999@gmail.com>
2021-08-08 16:20:40 +0300

    fgn

--- 2Dmatrix(search).py
+++ 2Dmatrix(search).py
@@ -18,6 +18,35 @@ class Solution:
 
         return False
 
+____________________________________________
+
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if not matrix: return False
+        row, col = len(matrix), len(matrix[0])
+        # edge case
+        if row == 0 or col == 0:
+            return False
+        r, c = 0, col - 1
+        for i in range(row):
+            if target <= matrix[i][c]:
+                # binary search
+                l, r = 0, col - 1
+                while l <= r:
+                    mid = (l + r) // 2
+                    if matrix[i][mid] == target:
+                        return True
+                    if target < matrix[i][mid]:
+                        r = mid - 1
+                    else:
+                        l = mid + 1
+                return False
+            else:
+                continue
+
+        return False
+
 
 
 
