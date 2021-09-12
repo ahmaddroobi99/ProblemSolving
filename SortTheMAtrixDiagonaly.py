b2e8bbd9afe8fa1d75d947e1177e01ffe744f7ea
ahmaddroobi99 <ahmad.droobi1999@gmail.com>
2021-09-12 15:45:38 +0300

    fgn

---
+++ SortTheMAtrixDiagonaly.py
@@ -0,0 +1,19 @@
+class Solution:
+    def diagonalSort(self, mat: List[List[int]]) -> List[List[int]]:
+        m = len(mat)
+        n = len(mat[0])
+
+        dict = defaultdict(list)
+
+        for i in range(m):
+            for j in range(n):
+                dict[i - j].append(mat[i][j])
+
+        for k in dict:
+            dict[k].sort(reverse=True)
+
+        for i in range(m):
+            for j in range(n):
+                mat[i][j] = dict[i - j].pop()
+
+        return mat
