2a8ff0c91b33b0401e4970267ffc38a68f0fdd13
ahmaddroobi99 <ahmad.droobi1999@gmail.com>
2021-11-06 08:40:18 +0200

    hg

---
+++ cheapest-flights-within-k-stops(sHORTESTpaTHalGORITHM.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        # graph problem
+        # find the minimum weight path
+        # use breadth-first search and downcount k until negative
+        keys = [float(inf)] * n
+        keys[src] = 0
+        f = collections.defaultdict(list)
+
+        for u, v, p in flights:
+            f[u].append((v, p))
+        # BFS
+        q = deque([(0, src)])
+        while k >= 0:
+            for _ in range(len(q)):
+                w, node = q.popleft()
+                for neighbor, price in f[node]:
+                    if w + price < keys[neighbor]:  # decrease key
+                        keys[neighbor] = w + price
+                        q.append((keys[neighbor], neighbor))
+            k -= 1
+
+        return keys[dst] if keys[dst] != float('inf') else -1
