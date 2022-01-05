9258f9897b6262d899c0de701078b576d0cff88e
ahmaddroobi99 <ahmad.droobi1999@gmail.com>
2022-01-05 18:35:54 +0200

    HappyNewYeaaaaaarrrr Most Beautiful day in my Life

---
+++ Brick Wall - Leetcode 554.py
@@ -0,0 +1,10 @@
+class Solution:
+    def leastBricks(self, wall: List[List[int]]) -> int:
+        countGaps = {0: 0}
+
+        for r in wall:
+            total = 0
+            for b in r[:-1]:
+                total += b
+                countGaps[total] = 1 + countGaps.get(total, 0)
+        return len(wall) - max(countGaps.values())
