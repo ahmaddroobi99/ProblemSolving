709d98800ec928f702dd7f668d2c53119c858be2
ahmaddroobi99 <ahmad.droobi1999@gmail.com>
2022-01-10 09:58:52 +0200

    HappyNewYeaaaaaarrrr Most Beautiful day in my Life

---
+++ AddBinary.py
@@ -0,0 +1,29 @@
+# Approach 1
+
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        carry = 0
+        result = ''
+
+        a = list(a)
+        b = list(b)
+
+        while a or b or carry:
+            if a:
+                carry += int(a.pop())
+            if b:
+                carry += int(b.pop())
+
+            result += str(carry %2)
+            carry //= 2
+
+        return result[::-1]
+# Approach 2
+class Solution:
+    def addBinary(self, a, b) -> str:
+        x, y = int(a, 2), int(b, 2)
+        while y:
+            answer = x ^ y
+            carry = (x & y) << 1
+            x, y = answer, carry
+        return bin(x)[2:]
