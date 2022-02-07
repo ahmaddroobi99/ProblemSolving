9c7f457e4bc9e1aa0c2434bc5b508547145e4e6f
ahmad.droubi <ahmad.droobi1999@gmail.com>
2022-02-07 10:00:58 +0200

    the New beginning

--- FindTheDifference.py
+++ FindTheDifference.py
@@ -14,3 +14,22 @@ class Solution:
         return list2[0]
 
     # solution 2 using hash maps its faster
+
+
+hashmap = {}
+
+for i in s:
+    if i in hashmap:
+        hashmap[i] += 1
+    else:
+        hashmap[i] = 1
+
+for i in t:
+    if i in hashmap:
+        hashmap[i] -= 1
+        if hashmap[i] == 0:
+            hashmap.pop(i)
+
+    else:
+        return i
+return hashmap.item[0]
